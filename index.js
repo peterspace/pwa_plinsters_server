a339dbff13e5ec0972417a58ff235df30f12c655
Peter Okeme <peterokeme@Peters-iMac.local>
2024-07-30 13:29:09 +0300

    updated cors 3

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 // app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+  next();
+});
+
 app.use(
   cors({
     origin: [
